5a619909f5e51c3b00e478c208467866866f4ad1
khanh290 <128286828+khanh290@users.noreply.github.com>
2023-04-16 17:57:58 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,258 +1,231 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.view.Window
 import android.widget.Button
 import android.widget.TextView
-import com.example.myapplication.R
-import java.text.DecimalFormat
-import java.util.*
-import java.util.regex.Pattern
 
-class MainActivity() : Activity() {
-    private var AC: Button? =null
-    var input: TextView? = null
-    var output: TextView? = null
-    var iptemp: TextView? = null
-    private var cong: Button? = null
-    private var tru: Button? = null
-    private var nhan: Button? = null
-    private var chia: Button? = null
-    private var result: Button? = null
-    private var num1: Button? = null
-    private var num2: Button? = null
-    private var num3: Button? = null
-    private var num4: Button? = null
-    private var num5: Button? = null
-    private var num7: Button? = null
-    private var num8: Button? = null
-    private var num9: Button? = null
-    private var num6: Button? = null
-    private var num0: Button? = null
+class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
+    private lateinit var btAC: Button
+    private lateinit var tvInput: TextView
+    private lateinit var cong: Button
+    private lateinit var tru: Button
+    private lateinit var nhan: Button
+    private lateinit var chia: Button
+    private lateinit var result: Button
+    private lateinit var btNum1: Button
+    private lateinit var btNum2: Button
+    private lateinit var btNum3: Button
+    private lateinit var btNum4: Button
+    private lateinit var btNum5: Button
+    private lateinit var btNum7: Button
+    private lateinit var btNum8: Button
+    private lateinit var btNum9: Button
+    private lateinit var btNum6: Button
+    private lateinit var btNum0: Button
+
+    var input1 = ""
+    var input2 = ""
+    var method = ""
+    val listResult: ArrayList<String> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
-        AnhXa()
-        AC!!.setOnClickListener { v: View? ->
-            input!!.text = ""
-            output!!.text = "0"
-            iptemp!!.text = ""
-        }
-
-        AC!!.setOnClickListener { v: View? ->
-            val str = StringBuffer(input!!.text)
-            if (str.length > 0) {
-                str.deleteCharAt(str.length - 1)
-                input!!.text = str
-                if (str.length > 0) Result() else output!!.text = "0"
-            }
-        }
-        num0!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "0"
-            Result()
-            iptemp!!.text = ""
-        }
-        num1!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "1"
-            Result()
-            iptemp!!.text = ""
-        }
-        num2!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "2"
-            Result()
-            iptemp!!.text = ""
-        }
-        num3!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "3"
-            Result()
-            iptemp!!.text = ""
-        }
-        num4!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "4"
-            Result()
-            iptemp!!.text = ""
-        }
-        num5!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "5"
-            Result()
-            iptemp!!.text = ""
-        }
-        num6!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "6"
-            Result()
-            iptemp!!.text = ""
-        }
-        num7!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "7"
-            Result()
-            iptemp!!.text = ""
-        }
-        num8!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "8"
-            Result()
-            iptemp!!.text = ""
-        }
-        num9!!.setOnClickListener { v: View? ->
-            input!!.text = input!!.text.toString() + "9"
-            iptemp!!.text = ""
-            Result()
-        }
-        chia!!.setOnClickListener { v: View? ->
-            if (input!!.text.length > 0) if (Character.isDigit(
-                    input!!.text[input!!.text.length - 1]
-                )
-            ) input!!.text = input!!.text.toString() + "."
-        }
-
-        tru!!.setOnClickListener { v: View? ->
-            iptemp!!.text = ""
-            if (output!!.text !== "0" && input!!.text === "") {
-                input!!.text = output!!.text.toString() + "-"
-            }
-            if (input!!.text.length == 0) input!!.text =
-                input!!.text.toString() + "-" else if (Character.isDigit(
-                    input!!.text[input!!.text.length - 1]
-                ) || input!!.text[input!!.text.length - 1] == '/' || input!!.text[input!!.text.length - 1] == 'x'
-            ) input!!.text = input!!.text.toString() + "-"
-        }
-        cong!!.setOnClickListener { v: View? ->
-            if (input!!.text.length > 0) if (Character.isDigit(
-                    input!!.text[input!!.text.length - 1]
-                )
-            ) input!!.text = input!!.text.toString() + "+"
-            if (output!!.text !== "0" && input!!.text === "") {
-                input!!.text = output!!.text.toString() + "+"
-            }
-        }
-        nhan!!.setOnClickListener { v: View? ->
-            if (input!!.text.length > 0) if (Character.isDigit(
-                    input!!.text[input!!.text.length - 1]
-                )
-            ) input!!.text = input!!.text.toString() + "x"
-            if (output!!.text !== "0" && input!!.text === "") {
-                input!!.text = output!!.text.toString() + "x"
-            }
-        }
-        chia!!.setOnClickListener { v: View? ->
-            if (input!!.text.length > 0) if (Character.isDigit(
-                    input!!.text[input!!.text.length - 1]
-                )
-            ) input!!.text = input!!.text.toString() + "/"
-            if (output!!.text !== "0" && input!!.text === "") {
-                input!!.text = output!!.text.toString() + "/"
-            }
-        }
-        result!!.setOnClickListener { v: View? ->
-            if (input!!.text.length > 1) Result()
-            iptemp!!.text = input!!.text as String + " ="
-            input!!.text = ""
-        }
+        anhXa()
+        setupUI()
     }
 
-    private var arrOperation: ArrayList<String>? = null
-    private var arrNumber: ArrayList<Double>? = null
-    fun addOperation(input: String) {
-        arrOperation = ArrayList()
-        addNumber(this.input!!.text.toString())
-        if (this.input!!.text.toString()) arrNumber!![0] = -arrNumber!![0]
-        val cArray = input.toCharArray()
-        git remote add origin https://github.com/khanh290/kh-nh.git
-        git branch -M main
-                git push -u origin main   var i = 1
-        while (i < cArray.size) {
-            when (cArray[i]) {
-                '+', '-' -> arrOperation!!.add(cArray[i].toString() + "")
-                'x', '/' -> {
-                    arrOperation!!.add(cArray[i].toString() + "")
-                    if (i < cArray.size - 1) if (cArray[i + 1] == '-' && i + 1 < cArray.size - 1) {
-                        arrNumber!![arrOperation!!.size] = -arrNumber!![arrOperation!!.size]
-                        i++
-                    }
-                }
-                else -> {}
-            }
-            i++
-        }
+    fun printInputLog() {
+        Log.e("MainActivity", "input1: " + input1 + "- method: " + method + " - input2: " + input2)
+
     }
 
-    fun addNumber(stringInput: String?) {
-        arrNumber = ArrayList()
-        val regex = Pattern.compile("(\\d+(?:\\.\\d+)?)")
-        val matcher = regex.matcher(stringInput)
-        while (matcher.find()) {
-            arrNumber!!.add(java.lang.Double.valueOf(Objects.requireNonNull(matcher.group(1))))
+    fun setupUI() {
+        btNum1.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "1"
+            if (method == "") {
+                input1 = input1 + "1"
+            } else {
+                input2 = input2 + "1"
+            }
+            printInputLog()
+
         }
+
+        btNum2.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "2"
+            if (method == "") {
+                input1 = input1 + "2"
+            } else {
+                input2 = input2 + "2"
+            }
+            printInputLog()
+        }
+
+        btNum3.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "3"
+            if (method == "") {
+                input1 = input1 + "3"
+            } else {
+                input2 = input2 + "3"
+            }
+            printInputLog()
+        }
+
+        btNum4.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "4"
+            if (method == "") {
+                input1 = input1 + "4"
+            } else {
+                input2 = input2 + "4"
+            }
+            printInputLog()
+        }
+
+        btNum5.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "5"
+            if (method == "") {
+                input1 = input1 + "5"
+            } else {
+                input2 = input2 + "5"
+            }
+            printInputLog()
+        }
+
+        btNum6.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "6"
+            if (method == "") {
+                input1 = input1 + "6"
+            } else {
+                input2 = input2 + "6"
+            }
+            printInputLog()
+        }
+
+        btNum7.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "7"
+            if (method == "") {
+                input1 = input1 + "7"
+            } else {
+                input2 = input2 + "7"
+            }
+            printInputLog()
+        }
+
+        btNum8.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "8"
+            if (method == "") {
+                input1 = input1 + "8"
+            } else {
+                input2 = input2 + "8"
+            }
+            printInputLog()
+        }
+
+        btNum9.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + "9"
+            if (method == "") {
+                input1 = input1 + "9"
+            } else {
+                input2 = input2 + "9"
+            }
+            printInputLog()
+        }
+
+
+
+        tru.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + " - "
+            method = "-"
+            printInputLog()
+        }
+
+        cong.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + " + "
+            method = "+"
+            printInputLog()
+        }
+
+        nhan.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + " * "
+            method = "*"
+            printInputLog()
+        }
+
+        chia.setOnClickListener {
+            val oldValue = tvInput.text.toString()
+            tvInput.text = oldValue + " / "
+            method = "/"
+            printInputLog()
+        }
+
+        result.setOnClickListener {
+            val value = getResult()
+            tvInput.text = value
+            listResult.add(value)
+        }
+
+
+        btAC.setOnClickListener {
+            input1 = ""
+            input2 = ""
+            method = ""
+            tvInput.text = ""
+            printInputLog()
+        }
+
     }
 
-    @SuppressLint("SetTextI18n")
-    fun Result() {
-        val df = DecimalFormat("###.#######")
-        var result: Double
-        addOperation(input!!.text.toString())
-        arrNumber!!.size
-        if (arrOperation!!.size > arrNumber!!.size) {
-            output!!.text = "Input error!!"
-        } else {
-            arrOperation!!.size
-            run {
-                var i = 0
-                while (i < arrNumber!!.size -1) {
-                    when (arrOperation!![i]) {
-                        "x" -> {
-                            arrNumber!![i] = arrNumber!![i] * arrNumber!![i + 1]
-                            arrNumber!!.removeAt(i + 1)
-                            arrOperation!!.removeAt(i)
-                            i--
-                        }
-                        "/" -> {
-                            arrNumber!![i] = arrNumber!![i] / arrNumber!![i + 1]
-                            arrNumber!!.removeAt(i + 1)
-                            arrOperation!!.removeAt(i)
-                            i--
-                        }
-                        else -> {}
-                    }
-                    i++
-                }
-            }
-            result = arrNumber!![0]
-            for (i in 0 until arrNumber!!.size - 1) {
-                when (arrOperation!![i]) {
-                    "+" -> result = result + arrNumber!![i + 1]
-                    "-" -> result = result - arrNumber!![i + 1]
-                    else -> {}
-                }
-            }
-            output!!.text = df.format(result) + ""
-        }
-    }
-
-    fun AnhXa() {
-        input = findViewById<View>(R.id.iptemp) as TextView
-        output = findViewById<View>(R.id.output) as TextView
-        AC = findViewById<View>(R.id.AC) as Button
+    fun anhXa() {
+        tvInput = findViewById<View>(R.id.input) as TextView
+        btAC = findViewById<View>(R.id.AC) as Button
         cong = findViewById<View>(R.id.daucong) as Button
         tru = findViewById<View>(R.id.dautru) as Button
         nhan = findViewById<View>(R.id.phepnhan) as Button
         chia = findViewById<View>(R.id.phepchia) as Button
         result = findViewById<View>(R.id.result) as Button
-        num0 = findViewById<View>(R.id.num0) as Button
-        num1 = findViewById<View>(R.id.num1) as Button
-        num2 = findViewById<View>(R.id.num2) as Button
-        num3 = findViewById<View>(R.id.num3) as Button
-        num4 = findViewById<View>(R.id.num4) as Button
-        num5 = findViewById<View>(R.id.num5) as Button
-        num6 = findViewById<View>(R.id.num6) as Button
-        num7 = findViewById<View>(R.id.num7) as Button
-        num8 = findViewById<View>(R.id.num8) as Button
-        num9 = findViewById<View>(R.id.num9) as Button
-        iptemp = findViewById<View>(R.id.iptemp) as TextView
+        btNum0 = findViewById<View>(R.id.num0) as Button
+        btNum1 = findViewById<View>(R.id.num1) as Button
+        btNum2 = findViewById<View>(R.id.num2) as Button
+        btNum3 = findViewById<View>(R.id.num3) as Button
+        btNum4 = findViewById<View>(R.id.num4) as Button
+        btNum5 = findViewById<View>(R.id.num5) as Button
+        btNum6 = findViewById<View>(R.id.num6) as Button
+        btNum7 = findViewById<View>(R.id.num7) as Button
+        btNum8 = findViewById<View>(R.id.num8) as Button
+        btNum9 = findViewById<View>(R.id.num9) as Button
+    }
+
+    fun getResult(): String {
+        var result = 0.0
+        if (method == "+") {
+            result = input1.toDouble() + input2.toDouble()
+        } else if (method == "-") {
+            result = input1.toDouble() - input2.toDouble()
+
+        } else if (method == "*") {
+        result = input1.toDouble() * input2.toDouble()
+        }
+        else if (method == "/") {
+        result = input1.toDouble() / input2.toDouble()
+        }
+        val oldValue = tvInput.text.toString()
+        return oldValue + " = " + result
     }
 }
